92aaf40236f90652f30bc6a5761e672c080b5dc3
Marcos Acosta <marcos@acosta.tv>
2012-09-02 00:17:33 -0500

    forget error handling

--- handler.rb
+++ handler.rb
@@ -7,12 +7,7 @@ end
 
 post '/' do
   @names = params[:names].split(/[\n\r]/).uniq.reject(&:empty?).each(&:strip!)
-
-  if params[:size].strip =~ /^\d+$/
-    @size = params[:size].strip.to_i
-  else
-    @size = @names.size
-  end
+  @size = params[:size].strip.to_i
 
   @groups = make_groups(@names.shuffle, @size)
 
